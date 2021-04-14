bec56bec9df584ef9c92e762def60f2db32a56e4
Drew Waldschmidt <drew@waldschmidt.com>
2021-04-14 01:19:20 -0400

    Save 3

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ Do the following:
 */
 
 let votingAge = 23;
+
 if (votingAge >= 18) {
     console.log('true') 
 }
@@ -35,9 +36,14 @@ Do the following:
    HINT: no function required
 */
 
+let first = 10; 
+let second = 20;
 
+if(second >= 15) {
+  first = 10 * second;
+}
 
-
+console.log(first)
 
 /*
 Task 1c - Convert Strings to Numbers
@@ -112,33 +118,33 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-let age = 2;
-let weight = 12;
-let x = 0.5;
-
-if (age >= 1) {
-  if (weight <= 5) {
-    x = 0.05;
-  } else if (weight > 5 && weight <=10) {
-    x = 0.04;
-  } else if (weight > 10 && weight <=15) {
-    x = 0.04;
-  } else if (weight > 15) {
-    x = 0.02;
-  } 
-} else {
-  if (age >= 2/12 && age < 4/12) {
-    x = 0.1;
-  } else if (age >= 4/12 && age < 7/12) {
-    x = 0.05;
-  } else if (age >= 7/12 && age < 12/12) {
-    x = 0.04;
+function hungryDog(weight,age) {
+  if (age >= 1) {
+    if (weight <= 5) {
+      return weight * 0.05;
+    } else if (weight > 5 && weight <=10) {
+      return weight * 0.04;
+    } else if (weight > 10 && weight <=15) {
+      return weight * 0.03;
+    } else if (weight > 15) {
+      return weight * 0.02;
+    } 
+  }
+  if (age < 1) { 
+      if (age >= 2/12 && age < 4/12) {
+      return weight * 0.10;
+    } else if (age >= 4/12 && age < 7/12) {
+      return weight * 0.05;
+    } else if (age >= 7/12 && age < 12/12) {
+      return weight * 0.04;
+    } else {
+    return 'please try again';
+    }
   }
 }
 
-function hungryDog(x, weight) {
-  return weight * x;
-}
+console.log(hungryDog(15,1));
+
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
 // Rock, Paper, Scissors - Let's play against the computer!
